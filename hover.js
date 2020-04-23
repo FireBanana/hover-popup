0b9c5d1f3aa0cbd85d0313551490e596f6a64ae8
Fire Banana <free0run@gmail.com>
2020-04-23 19:27:47 +0500

    Fixes and stuff

--- hover.js
+++ hover.js
@@ -4,60 +4,63 @@ const modalWidth = 400
 let modal = undefined
 let modalDescription = undefined
 let mHeight = undefined
+let mWidth = undefined
 let isHovering = false
 
-const modalHtml = 
-`<div id="modal-container">
-<p id="modal-description">This is a description which can be quite long so we need to keep it a bit short</p>
-</div>`
+const modalHtml =
+    `<div id="modal-window">
+        <div id="modal-container">
+            <p id="modal-description"></p>
+        </div>
+    </div>`
 
-function loaded(){
+function loaded() {
     document.body.innerHTML += modalHtml
     modal = document.getElementById('modal-container')
     modalDescription = document.getElementById('modal-description')
-    modal.style.width = `${modalWidth}px`
     let items = document.getElementsByClassName('modal-interact')
-    Array.from(items).forEach(element => {        
+    Array.from(items).forEach(element => {
         element.addEventListener('mouseover', mouseOver)
         element.addEventListener('mouseout', mouseOut)
     })
     modal.addEventListener('animationend', onAnimationEnd)
     modal.addEventListener('webkitAnimationEnd', onAnimationEnd)
+
+    modal.style.maxWidth = `${modalWidth}px`
 }
 
-function onAnimationEnd(event){
+function onAnimationEnd(event) {
     isHovering = false;
 }
 
-function mouseOver(event){
+function mouseOver(event) {
     let definition = event.target.getAttribute('data-modaldescription')
-    
-    if(definition == null)
+
+    if (definition == null)
         return
 
-    modal.style.display = 'block'
     modal.style.height = 'auto'
-    isHovering = true;
-    modalDescription.innerHTML = definition;
+    isHovering = true
+    modalDescription.innerHTML = definition
     modal.classList.remove('modal-animation')
     mHeight = parseFloat(getComputedStyle(modal).height)
+    mWidth = parseFloat(getComputedStyle(modal).width)
 }
 
-function mouseOut(event){
+function mouseOut(event) {
+    const w = modal.style.width
     modal.classList.add('modal-animation')
-    modalDescription.innerHTML = ""
+    modal.style.width = `${w}px`
     modal.style.height = `${mHeight}px`
 }
 
-function onMove(event){
-    if(!isHovering){
-        console.log('not hover')
+function onMove(event) {
+
+    if (!isHovering)
         return
-    }
 
     let x = event.clientX
-    let y = event.clientY     
+    let y = event.clientY
 
-    modal.style.left = x + modalWidth < window.innerWidth ? `${x}px` : `${x - modalWidth}px`
-    modal.style.top =  y + mHeight < window.innerHeight ? `${y}px` : `${y - mHeight}px`
+    modal.style.transform = `translate(${x + mWidth < document.documentElement.clientWidth ? x : x - mWidth}px, ${y + mHeight < document.documentElement.clientHeight ? y : y - mHeight}px)`
 }
